bc259cfb9a7c52403adb363edb0f92fd1ade76a5
anku598 <anik598@gmail.com>
2024-05-19 21:08:15 +0600

    latest and fix

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
         onClick={() => setIsOpen(!isOpen)}
       >
         <svg
-          class="w-5 h-5"
+          className="w-5 h-5"
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
